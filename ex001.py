672b15ca271194e982e2b2019aa480ee81b27c1b
pedrobenedetmachado <pedrohbm0102@gmail.com>
2025-09-03 21:33:35 -0300

    criei novas perguntas

---
+++ ex001.py
@@ -0,0 +1,9 @@
+nome=input("qual é seu nome?=")
+fazer=input("oque você gosta de fazer no tempo livre?")
+esporte=input("qual seu esporte preferido?=")
+comida=input("qual sua comida preferida?=")
+animais=input("você tem animais de estimação?=")
+print(f"prazer em conhece-lo, {nome}! seja bem vindo/a")
+
+
+
